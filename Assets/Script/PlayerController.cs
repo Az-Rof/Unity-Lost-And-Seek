55af9ff52d034cea857f0a2aa3157bb0b4b0b14f
Rofi Azhar <115684072+Az-Rof@users.noreply.github.com>
2024-10-27 06:19:42 +0700

    Adding Some Features

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private bool canDash = true;
     private bool isDashing;
-    [SerializeField] float dashingPower =4f;
+    [SerializeField] float dashingPower = 4f;
     [SerializeField] float dashingTime = 0.2f;
     [SerializeField] float dashingCooldown = 1f;
     Rigidbody2D rigidbody2D;
     Animator animator;
-    [SerializeField] float speed;
+    [SerializeField] float speed = 100f;
 
-    [SerializeField] float jumpforce;
+    [SerializeField] float jumpforce = 8f;
     [SerializeField] bool onground;
     [SerializeField] bool isjump;
     [SerializeField] bool isattack;
@@ -22,6 +23,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private TrailRenderer tr;
 
     public LayerMask groundLayer;
+
+
+    //Wall Jump
+    [SerializeField] private float wallJumpForce = 10f;
+    [SerializeField] private float wallSlideSpeed = 0.3f;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float wallCheckDistance = 0.5f;
+    private bool isWallSliding = false;
+    private bool isWallJumping = false;
+    private float wallJumpDirection = 0f;
+    private float wallJumpTime = 0.2f;
+    private float wallJumpTimer;
+
 
     void Awake()
     {
@@ -40,39 +54,58 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded();
-        jump();
-        attack();
         movement();
-        Dashing();
-        if (isDashing)
-        {
-            return;
-        }
+
     }
 
     void Update()
     {
-
+        jump();
+        attack();
+        Dashing();
+        WallSlide();
+        WallJump();
     }
+
+    private bool IsTouchingWall()
+    {
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, wallCheckDistance, wallLayer);
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, wallCheckDistance, wallLayer);
+
+        // Draw debug rays
+        Debug.DrawRay(transform.position, Vector2.right * wallCheckDistance, Color.red);
+        Debug.DrawRay(transform.position, Vector2.left * wallCheckDistance, Color.red);
+
+        return hitRight.collider != null || hitLeft.collider != null;
+    }
+
 
     void movement()
     {
-        float h = Input.GetAxis("Horizontal");
-        if (h != 0)
+        if (!isDashing && !isWallJumping)
         {
-            if (h > 0)
+            float h = Input.GetAxis("Horizontal");
+            if (h != 0)
             {
-                transform.localScale = new Vector3(1f, 1f, 1f);
+                if (h > 0)
+                {
+                    transform.localScale = new Vector3(1f, 1f, 1f);
+                }
+                else if (h < 0)
+                {
+                    transform.localScale = new Vector3(-1f, 1f, 1f);
+                }
+                //rigidbody2D.velocity = new Vector2(h, rigidbody2D.velocity.y) * speed * Time.deltaTime;
+
+                Vector2 newVelocity = rigidbody2D.velocity;
+                newVelocity.x = h * speed * Time.deltaTime;
+                rigidbody2D.velocity = newVelocity;
+                animator.SetFloat("hMove", MathF.Abs(h));
+                animator.SetFloat("hSpeed", MathF.Abs(speed) * MathF.Abs(h));
             }
-            else if (h < 0)
-            {
-                transform.localScale = new Vector3(-1f, 1f, 1f);
-            }
-            rigidbody2D.velocity = new Vector2(h, rigidbody2D.velocity.y) * speed * Time.deltaTime;
-            animator.SetFloat("hMove", MathF.Abs(h));
-            animator.SetFloat("hSpeed", MathF.Abs(speed) * MathF.Abs(h));
+            animator.SetFloat("yMove", MathF.Abs(rigidbody2D.velocity.y));
         }
-        animator.SetFloat("yMove", MathF.Abs(rigidbody2D.velocity.y));
+
     }
 
     void jump()
@@ -87,7 +120,7 @@ public class PlayerController : MonoBehaviour
     {
         animator.SetBool("onGround", false);
         yield return new WaitForSeconds(0.25f);
-        rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, jumpforce), ForceMode2D.Impulse);
+        rigidbody2D.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
     }
 
     void isGrounded()
@@ -115,6 +148,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             animator.SetTrigger("isAttack");
+
         }
     }
 
@@ -126,6 +160,9 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(Dash());
         }
     }
+
+
+
     IEnumerator Dash()
     {
         canDash = false;
@@ -137,6 +174,65 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(dashingTime);
         tr.emitting = false;
         rigidbody2D.gravityScale = originalGravity;
+        isDashing = false; // End the dash
+        rigidbody2D.velocity = Vector2.zero;
+        //yield return new WaitForSeconds(dashingCooldown - dashingTime);
         canDash = true;
+    }
+
+
+    void WallSlide()
+    {
+        if (IsTouchingWall() && !onground && rigidbody2D.velocity.y < 0)
+        {
+            isWallSliding = true;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Clamp(rigidbody2D.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void WallJump()
+    {
+        if (isWallSliding)
+        {
+            // Determine the direction of the wall
+            float wallDirection = 0f;
+            if (Physics2D.Raycast(transform.position, Vector2.right, wallCheckDistance, wallLayer))
+            {
+                wallDirection = -1f; // Wall is on the right
+            }
+            else if (Physics2D.Raycast(transform.position, Vector2.left, wallCheckDistance, wallLayer))
+            {
+                wallDirection = 1f; // Wall is on the left
+            }
+
+            // Update player scale to face the wall
+            if (wallDirection != 0)
+            {
+                transform.localScale = new Vector3(wallDirection, 1f, 1f);
+            }
+
+            isWallJumping = false;
+            wallJumpDirection = wallDirection; // Use the wall direction for jumping
+            wallJumpTimer = wallJumpTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isWallJumping = true;
+                rigidbody2D.velocity = new Vector2(wallJumpDirection * wallJumpForce, jumpforce);
+                Invoke(nameof(StopWallJumping), wallJumpTime);
+            }
+        }
+    }
+
+
+    void StopWallJumping()
+    {
+        isWallJumping = false;
     }
 }
